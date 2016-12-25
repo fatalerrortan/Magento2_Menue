0ff6c21978be703cac31cf3060654fb0b3d07b10
fatalerrortan <tiemann9898@gmail.com>
2016-12-25 03:15:43 +0100

    25/12/2016 implemented funcs for generating html wrapper for each products

--- app/code/Nextorder/Menue/Block/Frontend/Menue.php
+++ app/code/Nextorder/Menue/Block/Frontend/Menue.php
@@ -1,21 +1,66 @@
 <?php
 namespace Nextorder\Menue\Block\Frontend;
 
-class Menue extends \Magento\Framework\View\Element\Template
-{
-//    load custom helper in block class
+class Menue extends \Magento\Framework\View\Element\Template{
+
+    protected $_logger;
+    public $_helper;
+    protected $_productCollection;
+
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Nextorder\Menue\Helper\Data $helper,
+        \Magento\Framework\View\Element\Template\Context $context, //parent block injection
+        \Nextorder\Menue\Helper\Data $helper, //helper injection
+        \Psr\Log\LoggerInterface $logger, //log injection
+        \Magento\Catalog\Model\ProductFactory $productCollection, //product collection injection
         array $data = []
-    ) {
-        $this->helper = $helper;
+    )
+    {
+        $this->_helper = $helper;
+        $this->_logger = $logger;
+        $this->_productCollection = $productCollection->create();
         parent::__construct($context, $data);
     }
+    /*
+     * load Predefined Products in Weekend Menu
+     */
+    public function loadProductHtmlBySku(){
 
+        $products = $this->_helper->getAdminConfig();
+        $html = '';
+        foreach ($products as $item) {
+            $product = $this->_productCollection->loadByAttribute('sku', $item);
+//            $this->_logger->addDebug($this->getUrl('pub/media/catalog').'product'.$product->getImage());
+            $productName = $product->getName();
+            $productPrice = $product->getPrice();
+            $imgUrl = $this->getUrl('pub/media/catalog').'product'.$product->getImage();
+            $productShortDescription = $product->getShortDescription();
+            $priceClass = $product->getData('price_class');
+            $html .= $this->getHtml($productName, $productPrice, $priceClass, $productShortDescription, $imgUrl);
+        }
+        return $html;
+    }
+    /*
+     * load html wrapper for each product
+     */
+    public function getHtml($name, $price, $priceClass, $description, $imgUrl){
 
-    public function getHelloWorldTxt(){
-
-        return 'Hello world!';
+        $html = "<tr>
+                <td class='img_container'>
+                    <img src='".$imgUrl."' scrset='".$imgUrl."' alt='".$name."' width='200px' height='200px' />
+                    <h5>".$name."</h5>
+                </td> 
+                <td class='product_info'>
+                    <div class='product_content'>
+                        <span>".$description."</span>
+                    </div>
+                    <div class='diy_button'>
+                        <button price_class='".$priceClass."'>Austausch</button>
+                    </div>
+                 </td>
+                 <td class='product_price'>
+                    <span>".$price."&euro;</span>
+                 </td>   
+               </tr>";
+        return $html;
     }
 }
--- app/code/Nextorder/Menue/Helper/Data.php
+++ app/code/Nextorder/Menue/Helper/Data.php
@@ -9,8 +9,14 @@ namespace Nextorder\Menue\Helper;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    public function testHelper()
-    {
-        return "testHelper_Xulin";
+    public function getAdminConfig(){
+        return array(
+            1 => 'test_sku_01',
+            2 => 'test_sku_02',
+            3 => 'test_sku_03',
+            4 => 'test_sku_04',
+            5 => 'test_sku_05',
+            6 => 'test_sku_06'
+        );
     }
 }
